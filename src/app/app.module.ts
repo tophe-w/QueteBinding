1f1fd577375c47816c815b2c2309200abcc60bfd
WALLET Christophe (SNCF VOYAGEURS / DIRECTION DE LIGNES CNU / PROD_PCAT_TN) <8106550t@commun.ad.sncf.fr>
2023-05-15 15:54:40 +0200

    ajout des fichiers de la quete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
